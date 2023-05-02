fe0c71dae3d8c8a0c19ef4dca6de5c9184251358
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-05-02 13:43:00 -0400

    setup new entry form

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,16 @@
-import type { V2_MetaFunction } from '@remix-run/node';
+import type { ActionArgs, V2_MetaFunction } from '@remix-run/node';
+import { redirect } from '@remix-run/node';
+import { Form } from '@remix-run/react';
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: 'Work Journal' }];
 };
+
+export async function action({ request }: ActionArgs) {
+  const formData = await request.formData();
+  console.log(Object.fromEntries(formData));
+  return redirect('/');
+}
 
 export default function Index() {
   return (
@@ -11,6 +19,65 @@ export default function Index() {
       <p className="mt-2 text-lg text-gray-400">
         Learnings and doings. Updated weekly
       </p>
+
+      <div className="my-8 border p-2">
+        <Form method="post">
+          <p className="italic">Create an entry</p>
+
+          <div>
+            <div className="mt-4">
+              <input type="date" name="date" className="text-gray-700" />
+            </div>
+
+            <div className="mt-2 space-x-6">
+              <label>
+                <input
+                  className="mr-1 "
+                  type="radio"
+                  name="category"
+                  value="work"
+                />
+                Work
+              </label>
+              <label>
+                <input
+                  className="mr-1 "
+                  type="radio"
+                  name="category"
+                  value="learning"
+                />
+                Learning
+              </label>
+              <label>
+                <input
+                  className="mr-1 "
+                  type="radio"
+                  name="category"
+                  value="interesting-thing"
+                />
+                Interesting thing
+              </label>
+            </div>
+
+            <div className="mt-2">
+              <textarea
+                name="text"
+                className="w-full text-gray-700"
+                placeholder="Write you entry..."
+              />
+            </div>
+
+            <div className="mt-1 text-right">
+              <button
+                className="bg-blue-500 px-4 py-1 font-medium text-white"
+                type="submit"
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </Form>
+      </div>
 
       <div className="mt-4">
         <p className="font-bold">
